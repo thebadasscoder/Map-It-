b1cbc5747deb2f52e3ea8a5aa48ff82abf9c3cc6
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2017-08-15 19:54:43 -0400

    chnages made

--- front/app.jsx
+++ front/app.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute} from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
 
+const customHistory = createBrowserHistory()
+
+//Components 
+import Maps from './map.jsx';
 
 const App = React.createClass({
 	render(){
 		return(
 			<div>
-			<h1> Hello World! </h1>
+			<center><h1>Food Hunt</h1></center>
+			{this.props.children}
 			</div>
 		)
 	}
 })
 
 ReactDOM.render(
-<App/>,
+<Router history={customHistory}>
+	<Route exact={true}path="/" component={App}>
+		<Route path="/map" component={Maps}/>
+	</Route>
+</Router>,
 document.getElementById('root')
 )
 
---
+++ front/map.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import $ from 'jquery';
+
+
+const Maps = React.createClass({
+	render(){
+		return(
+		<div>
+			<h1> Map Page </h1>
+		</div>
+		)
+	}
+});
+
+export default Maps;
